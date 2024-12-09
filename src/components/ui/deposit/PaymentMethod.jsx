27ec3ed8dac50f8d6a21746e925559debcbe0ddd
sabbirHossain <sabbirshnt@gmail.com>
2024-12-09 10:55:40 +0600

    done

--- src/components/ui/deposit/PaymentMethod.jsx
+++ src/components/ui/deposit/PaymentMethod.jsx
@@ -499,56 +499,60 @@ const PaymentMethod = ({
           )}
 
           {/* buttons */}
-          <div className="flex items-start justify-center gap-x-2 py-3 px-5">
-            <div className="inline-flex items-center">
-              <label
-                className="relative flex cursor-pointer items-center rounded-full bg-bg_Quinary"
-                htmlFor="blue"
-              >
-                <input
-                  className="before:content[''] before:bg-bg_Secondary3 rounded-md peer relative cursor-pointer appearance-none border border-success transition-all before:absolute before:left-2/4 before:top-2/4 before:block before:h-max before:w-max before:-translate-x-2/4 before:-translate-y-2/4 before:opacity-0 before:transition-opacity checked:border-success checked:bg-bg_Success hover:before:opacity-10 h-5 w-5"
-                  id="blue"
-                  type="checkbox"
-                  defaultChecked
-                />
-                <span className="pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-text_Quaternary opacity-0 transition-opacity peer-checked:opacity-100">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-3.5 w-3.5"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    stroke="currentColor"
-                    strokeWidth="1"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
+          {paymentMethods?.length > 0 && (
+            <div className="flex items-start justify-center gap-x-2 py-3 px-5">
+              <div className="inline-flex items-center">
+                <label
+                  className="relative flex cursor-pointer items-center rounded-full bg-bg_Quinary"
+                  htmlFor="blue"
+                >
+                  <input
+                    className="before:content[''] before:bg-bg_Secondary3 rounded-md peer relative cursor-pointer appearance-none border border-success transition-all before:absolute before:left-2/4 before:top-2/4 before:block before:h-max before:w-max before:-translate-x-2/4 before:-translate-y-2/4 before:opacity-0 before:transition-opacity checked:border-success checked:bg-bg_Success hover:before:opacity-10 h-5 w-5"
+                    id="blue"
+                    type="checkbox"
+                    defaultChecked
+                  />
+                  <span className="pointer-events-none absolute left-2/4 top-2/4 -translate-x-2/4 -translate-y-2/4 text-text_Quaternary opacity-0 transition-opacity peer-checked:opacity-100">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-3.5 w-3.5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      stroke="currentColor"
+                      strokeWidth="1"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </span>
+                </label>
+              </div>
+              <span className="text-sm text-textColor font-lato font-[400] leading-5">
+                I have read and agree with{" "}
+                <span className="text-text_Primary underline text-sm font-lato font-[400] leading-4 cursor-pointer">
+                  the terms of payment and withdrawal policy.
                 </span>
-              </label>
-            </div>
-            <span className="text-sm text-textColor font-lato font-[400] leading-5">
-              I have read and agree with{" "}
-              <span className="text-text_Primary underline text-sm font-lato font-[400] leading-4 cursor-pointer">
-                the terms of payment and withdrawal policy.
               </span>
-            </span>
-          </div>
-          <div className="w-full text-center py-4 px-2">
-            <button
-              disabled={paymentMethods?.length === 0 || !tabs}
-              onClick={() => {
-                setPaymentMethods(false);
-                setUploadTransaction(true);
-              }}
-              type="submit"
-              className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out bg-bg_Primary flex items-center justify-center gap-x-2 w-full text-text_Quaternary h-10 text-base rounded-md font-[500] leading-4 disabled:bg-bg_Quinary cursor-pointer"
-            >
-              <span>I have made the payment</span>
-            </button>
-          </div>
+            </div>
+          )}
+          {paymentMethods?.length > 0 && (
+            <div className="w-full text-center py-4 px-2">
+              <button
+                disabled={paymentMethods?.length === 0 || !tabs}
+                onClick={() => {
+                  setPaymentMethods(false);
+                  setUploadTransaction(true);
+                }}
+                type="submit"
+                className="inline-block leading-normal relative overflow-hidden transition duration-150 ease-in-out bg-bg_Primary flex items-center justify-center gap-x-2 w-full text-text_Quaternary h-10 text-base rounded-md font-[500] leading-4 disabled:bg-bg_Quinary cursor-pointer"
+              >
+                <span>I have made the payment</span>
+              </button>
+            </div>
+          )}
         </form>
       </div>
     </div>
